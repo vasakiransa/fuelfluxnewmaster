9dd0fb8f88e7281ae2fa51be6a2be1979c2df762
Akash <cosmichipster509@gmail.com>
2024-09-20 19:38:31 +0530

    Updated modal for Products and improved mobile view for Terms and Privacy

--- script.js
+++ script.js
@@ -2,3 +2,42 @@ const navDialog = document.getElementById("nav-dialog");
 function handleMenu() {
   navDialog.classList.toggle("hidden");
 }
+
+function openModal() {
+  document.getElementById("productsModal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("productsModal").classList.add("hidden");
+}
+
+function showPersonal() {
+  document.getElementById("personalForm").classList.remove("hidden");
+  document.getElementById("businessForm").classList.add("hidden");
+}
+
+function showBusiness() {
+  document.getElementById("businessForm").classList.remove("hidden");
+  document.getElementById("personalForm").classList.add("hidden");
+}
+
+function toggleFAQ(id) {
+  const content = document.getElementById(id);
+  const icon = document.getElementById("icon-" + id);
+
+  if (content.classList.contains("hidden")) {
+    content.classList.remove("hidden");
+    icon.classList.add("rotate-180");
+  } else {
+    content.classList.add("hidden");
+    icon.classList.remove("rotate-180");
+  }
+}
+
+function openTermsModal() {
+  document.getElementById("termsModal").classList.remove("hidden");
+}
+
+function closeTermsModal() {
+  document.getElementById("termsModal").classList.add("hidden");
+}
